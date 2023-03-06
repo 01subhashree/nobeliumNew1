d5f662bd2d0ee1c8b9c6e40db642a4fa7a7aa487
01subhashree <ssubhashree094@gmail.com>
2023-03-06 22:01:14 +0530

    push the code to github account

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+export default App;
+function Heading1() {
+  return (
+    <h1>I am heading one ... from first component</h1>
+  )
+}
+function Heading2() {
+  return (
+    <h2>I am heading two ... from second component</h2>
+  )
+}
+function Heading3() {
+  return (
+    <h3>I am heading three ... from third component</h3>
+  )
+}
+function Button() {
+  return (
+    <button>click me!!</button>
+  )
+
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Heading1 />
+      <Heading2 />
+      <Heading3 />
+      <Button />
     </div>
-  );
+  )
 }
-
-export default App;
